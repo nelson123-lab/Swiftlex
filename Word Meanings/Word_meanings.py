d4d878ba425d93c88145284972a74184058381ad
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-16 21:01:16 -0500

    m

--- Word Meanings/Word_meanings.py
+++ Word Meanings/Word_meanings.py
@@ -1,6 +1,9 @@
 from PyDictionary import PyDictionary
+from gtts import gTTS
+import os
 # Meaning of Word using dictionary1
-a = input('Enter word')
+# The text that you want to convert to audio
+mytext = input('Enter word')
 dictionary1 = PyDictionary()
 # Finding Synonym using Dictionary1
 print(dictionary1.meaning('Archive'))
@@ -8,3 +11,11 @@ print(dictionary1.meaning('Archive'))
 """
 {'Noun': ['a depository containing historical records and documents'], 'Verb': ['put into an archive']}
 """
+# Language in which you want to convert
+language = 'en'
+myobj = gTTS(text=mytext, lang=language, slow=False)
+# Saving the converted audio in a mp3 file named
+# welcome
+myobj.save("welcome.mp3")
+# Playing the converted file
+os.system("mpg321 welcome.mp3")
