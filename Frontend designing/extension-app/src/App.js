ec04af8156ae52cee49619ef70d10c342f9fee2a
Vyshakh Anil <vyshakhanil000@gmail.com>
2023-06-09 23:43:36 +0530

    front-end changes

---
+++ Frontend designing/extension-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CssProject from "./Components/CssProject";
+
+function App() {
+  return (
+    <div className="App">
+      <CssProject />
+    </div>
+  );
+}
+
+export default App;
